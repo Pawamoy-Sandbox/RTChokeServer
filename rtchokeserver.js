13c924aecb6dbff468c19370071ffae9401c7779
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2014-11-09 16:41:25 +0100

    using view engine rederer for routes

--- rtchokeserver.js
+++ rtchokeserver.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 
 var app = express();
 
@@ -13,20 +13,23 @@ app.set('view engine', 'handlebars');
 // we start to listen to port 3000
 app.set('port', process.env.PORT || 3000);
 
+// basic routes for website pages
+app.get('/', function(req, res){
+    res.render('home');
+})
+
 
 // we redirect the HTTP requests
 app.use(function(req, res){
-    res.status(404)
-    res.type('text/plain')
-    res.send('404')
+    res.status(404);
+    res.render('404');
 });
 
 
 app.use(function(req, res){
-    console.error(err.stack)
-    res.status(500)
-    res.type('text/plain')
-    res.send('500')
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 
