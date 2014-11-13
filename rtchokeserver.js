af745df1a60d4ef081d6466ada53f4fec3bc2aee
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2014-11-13 14:12:19 +0100

    http redirection code, trying to fix them

--- rtchokeserver.js
+++ rtchokeserver.js
@@ -72,13 +72,13 @@ app.get('/signup', function(req, res){
 //----------------------------------------------------------------------------------
 // we redirect the HTTP requests
 //----------------------------------------------------------------------------------
-app.use(function(req, res){
+app.use(function(req, res, next){
     res.status(404);
     res.render('404');
 });
 
 
-app.use(function(req, res){
+app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500);
     res.render('500');
