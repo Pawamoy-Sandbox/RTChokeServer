c042b8b1fda4c58dd11a47f0b101185a6b3d1862
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2014-11-18 15:59:40 +0100

    correcting error from oauth2 module

--- handlers/oauth2.js
+++ handlers/oauth2.js
@@ -1,12 +1,11 @@
+
 var credentials = require('./credentials.js');
 
 var gapi = require('googleapis');
 var OAuth2 = gapi.auth.OAuth2;
 
-var oa2Client = new OAuth2(credentials.oauth2.googleapi.clientID,
-                          credentials.oauth2.googleapi.clientSecret,
-                          credentials.oauth2.googleapi.redirectURI);
+exports.oa2Client = new OAuth2(credentials.oauth2.googleapi.clientID,
+                               credentials.oauth2.googleapi.clientSecret,
+                               credentials.oauth2.googleapi.redirectURI);
 
-var plus = gapi.plus('v1');
-
-
+exports.plus = gapi.plus('v1');
--- handlers/route.js
+++ handlers/route.js
@@ -47,7 +47,7 @@ module.exports = function(app){
     });
 
     app.get('/oauth2', function(req, res){
-        var authUrlGoogle = oa2Client.generateAuthUrl({
+        var authUrlGoogle = oauth2.oa2Client.generateAuthUrl({
             access_type: 'offline',
             scope: 'https://www.googleapis.com/auth/plus.me'
         });
@@ -60,8 +60,8 @@ module.exports = function(app){
     app.get('/oauth2callback', function(req, res){
         var code = req.query.code;
 
-        oa2Client.getToken(code, function(err, tokens){
-            oa2Client.setCredentials(tokens);
+        oauth2.oa2Client.getToken(code, function(err, tokens){
+            oauth2.oa2Client.setCredentials(tokens);
             retrieveGooglePlusProfile();
         });
         var locals = {
@@ -73,7 +73,7 @@ module.exports = function(app){
 
 
     var retrieveGooglePlusProfile = function(){
-        plus.people.get({ userId: 'me', auth: oa2Client }, function (err, profile){
+        oauth2.plus.people.get({ userId: 'me', auth: oauth2.oa2Client }, function (err, profile){
             if (err){
                 console.log('Error while fetching for google+ profile', err);
                 return;
@@ -82,8 +82,6 @@ module.exports = function(app){
             urlPicture = profile.image.url;
         });
     };
-
-
 
     //----------------------------------------------------------------------------------
     // we redirect the HTTP requests
