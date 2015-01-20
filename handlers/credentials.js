7a421489c9e88a0fc749dec0a86fefa380ba967e
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-01-20 16:37:43 +0100

    added cred, some stress tests (on stream and map)

---
+++ handlers/credentials.js
@@ -0,0 +1,31 @@
+module.exports = {
+    mongo: {
+        development: {
+            connectionString: 'mongodb://rtchokeuser:rtchokeuser@ds063909.mongolab.com:63909/rtchoke',
+        },
+        production: {
+            connectionString: 'mongodb://rtchokeuser:rtchokeuser@ds063909.mongolab.com:63909/rtchoke',
+        },
+    },
+
+    oauth2: {
+        googleapi: {
+            clientID: '216484220053-5dcr6r7oij34rt6d6cbtgrfro3jn55n4.apps.googleusercontent.com',
+            clientSecret: 'MDpv02iBy9zKwDR4mSbF9C-m',
+            redirectURI: 'http://rtchoke-dev.u-strasbg.fr:3000/auth/google/callback',
+        },
+        facebook: {
+            clientID: '1515926505330705',
+            clientSecret: '8ee226f5a1753a22304f8441ae25ccf7',
+            redirectURI: 'http://rtchoke-dev.u-strasbg.fr:3000/auth/facebook/callback',
+        },
+        twitter: {
+            clientID: 'AVFlZ6KmiQ4lr9dKxnxkovQu4',
+            clientSecret: 'Iy7lojeg1wTX9l9wseqHOtpGtnhaMKyfUX4jzmJbErWjpDAAR9',
+            redirectURI: 'http://rtchoke-dev.u-strasbg.fr:3000/auth/twitter/callback',
+        },
+
+
+    },
+};
+
--- qa/tests-stress.js
+++ qa/tests-stress.js
@@ -1,13 +1,15 @@
 var loadtest = require('loadtest');
-
 var expect = require('chai').expect;
+
+var parallel = 4;
+var maxRequ = 250;
 
 suite('Stress tests', function(){
     test('Homepage handling 250 requests', function(done){
         var options = {
             url: 'http://localhost:3000',
-            concurrency: 4,
-            maxRequests: 250
+            concurrency: parallel,
+            maxRequests: maxRequ
         };
         loadtest.loadTest(options, function(err,result){
             expect(!err);
@@ -18,8 +20,8 @@ suite('Stress tests', function(){
     test('View stream handling 250 requests', function(done){
         var options = {
             url: 'http://localhost:3000/viewstream',
-            concurrency: 4,
-            maxRequests: 250
+            concurrency: parallel,
+            maxRequests: maxRequ
         };
         loadtest.loadTest(options, function(err,result){
             expect(!err);
@@ -27,6 +29,29 @@ suite('Stress tests', function(){
             done();
         });
     });
-
+    test('View stream handling 250 requests', function(done){
+        var options = {
+            url: 'http://localhost:3000/stream',
+            concurrency: parallel,
+            maxRequests: maxRequ
+        };
+        loadtest.loadTest(options, function(err,result){
+            expect(!err);
+            expect(result.totalTimeSeconds < 1);
+            done();
+        });
+    });
+    test('View stream handling 250 requests', function(done){
+        var options = {
+            url: 'http://localhost:3000/map',
+            concurrency: parallel,
+            maxRequests: maxRequ
+        };
+        loadtest.loadTest(options, function(err,result){
+            expect(!err);
+            expect(result.totalTimeSeconds < 1);
+            done();
+        });
+    });
 });
 
