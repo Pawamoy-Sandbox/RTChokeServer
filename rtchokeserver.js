b90e4a7ae66127eb69428cf086ea17c17276d2b6
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-01-20 16:10:48 +0100

    added debug info in console for webstorm

--- rtchokeserver.js
+++ rtchokeserver.js
@@ -5,12 +5,19 @@ var passport        = require('passport');
 var flash           = require('connect-flash');
 var cookieParser    = require('cookie-parser');
 var session         = require('express-session');
-var app             = express();
+var logger          = require('morgan');
 var credentials     = require('./handlers/credentials.js');
 
+var app             = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cookieParser());
 
 app.use(session({secret: '<mysecret>',
                  saveUninitialized: true,
@@ -41,4 +48,28 @@ require('./handlers/handlebars.js')(app);
 app.listen(app.get('port'), function(){
     console.log('RTChokeServer (' + app.get('env') +
                 ') started on port ' + app.get('port'));
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
