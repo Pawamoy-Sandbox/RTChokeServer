b777aeca2fe3ce9f930b941f08c2627c7de410c2
tersfeld <thomas.ersfeld@gmail.com>
2015-01-26 16:59:19 +0100

    APICall: launchStream(). We have now to connect this call to this API Call when you click on the Start button

--- handlers/route_api.js
+++ handlers/route_api.js
@@ -1,6 +1,22 @@
 module.exports = function(app){
 
-    app.post('/api_getusercoordinates/:userId/:coordinates', function (req, res){
+    var User = require('../models/user.js');
 
+    app.post('/api_setUserCoordinates/:userId/:coordinates', function (req, res){
+
+    });
+
+    app.put('/api_launchStream/:userId', function(req,res) {
+        User.findOne({'_id': userId}, function (err, user) {
+            // if there are any errors, return the error
+            if (err)
+                return done(err);
+
+            // if a user is valid, he is now streaming
+            if (user) {
+                user.isStreaming = true;
+                return done(user);
+            }
+        });
     });
 };
