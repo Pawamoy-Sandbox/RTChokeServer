8b57b56202622463d48c3eb76cf07cc28bb549a1
Madalina <mady@ubuntu.(none)>
2015-02-04 10:53:50 -0800

    First version of chat

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 'use strict';
 
+var ent = require('ent');
+var fs = require('fs');
 var app = require('./index');
-var http = require('http');
 
 
-var server;
+//var server;
 
 /*
  * Create and start HTTP server.
  */
 
-server = http.createServer(app);
+var http = require('http');
+var fs = require('fs');
+// Loading the index file . html displayed to the client
+var server = http.createServer(app);
+// Loading socket.io
+var io = require('socket.io').listen(server);
+// When a client connects, we note it in the console
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', 'You are connected!');
+    socket.broadcast.emit('message', 'Another user has just connected!');
+
+    socket.on('message', function (message) {
+		socket.broadcast.emit('message', message);
+    }); 
+});
 server.listen(process.env.PORT || 3000);
+
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
