a159899c0027bd97874b8771b89c49e303210052
tersfeld <thomas.ersfeld@gmail.com>
2015-02-10 14:47:26 +0100

    Fixing bug: we don't have to be authenticated to go to index route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ module.exports = function(app){
         res.render('index');
     });
 
-    app.get('/index', helpers.ensureAuthenticated, function(req, res){
+    app.get('/index', function(req, res){
         res.render('index');
     });
 
