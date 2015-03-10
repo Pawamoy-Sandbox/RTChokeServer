1164daafb85d57f7fb87902f26c1c8d6c369f708
Madalina <mady@ubuntu.(none)>
2015-03-10 07:07:23 -0700

    Added username to chat messages.

--- server.js
+++ server.js
@@ -18,12 +18,17 @@ var server = http.createServer(app);
 // Loading socket.io
 var io = require('socket.io').listen(server);
 // When a client connects, we note it in the console
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', 'You are connected!');
-    socket.broadcast.emit('message', 'Another user has just connected!');
+io.sockets.on('connection', function (socket, username) {
+
+ socket.on('new_client', function(username) {
+        username = ent.encode(username);
+        socket.username = username;
+        socket.emit('connected','You are connected!');
+        socket.broadcast.emit('new_client', username);
+    });
 
     socket.on('message', function (message) {
-		socket.broadcast.emit('message', message);
+		socket.broadcast.emit('message', {username: socket.username, message: message});
     }); 
 });
 server.listen(process.env.PORT || 3000);
