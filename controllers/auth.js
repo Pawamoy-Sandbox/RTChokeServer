c169af768e727f9f30a86908eb9e6b04b43c3310
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-03-24 16:40:09 +0100

    Fix app -> server

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,28 +4,28 @@ module.exports = function (server) {
     var passport = require('passport');
     var helpers = require('../lib/helpers.js');
 
-    app.get('/auth', function (req, res) {
+    server.get('/auth', function (req, res) {
         res.render('auth');
     });
 
-    app.post('/process', passport.authenticate('local-signup', {
+    server.post('/process', passport.authenticate('local-signup', {
         successRedirect: '/index',
         failureRedirect: '/signup'
     }));
 
-    app.post('/login', passport.authenticate('local-login', {
+    server.post('/login', passport.authenticate('local-login', {
         successRedirect: '/index',
         failureRedirect: '/index'
     }));
 
-    app.get('/logout', helpers.ensureAuthenticated, function (req, res) {
+    server.get('/logout', helpers.ensureAuthenticated, function (req, res) {
         req.logout();
         req.session.user = undefined;
         res.locals.user = undefined;
         res.render('index');
     });
 
-    app.get('/signup', function (req, res) {
+    server.get('/signup', function (req, res) {
         res.render('signup');
     });
 };
